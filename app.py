38ad3398bb2b4746214591de756ce437a3eb8ed4
Saleck07 <salekamein@gmail.com>
2025-02-11 02:11:27 +0000

    mokhtar

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'Hello mOTHERFUCKER'
+    return 'Hello MOKHTAR'
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
